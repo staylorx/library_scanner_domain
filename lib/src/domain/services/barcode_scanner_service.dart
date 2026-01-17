ac5868213e6e0336eb9f3cd6ad1971ba0a140ab6
Stephen Taylor <staylorx@gmail.com>
2026-01-17 10:15:39 -0700

    refactor: Rename abstract class for barcode scanning service to improve clarity; update version in pubspec.yaml

--- lib/src/domain/services/barcode_scanner_service.dart
+++ lib/src/domain/services/barcode_scanner_service.dart
@@ -1,8 +1,8 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:library_scanner_domain/library_scanner_domain.dart';
 
-/// Service for barcode scanning operations.
-abstract class AbstractBarcodeScannerService {
+/// Abstract service for barcode scanning operations.
+abstract class BarcodeScannerService {
   /// Starts scanning for barcodes and returns the scanned code if successful.
   Future<Either<Failure, String?>> startScanning();
 
